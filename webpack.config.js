e420b2371e0431b8c6ef2ef23eb2c149f60d3716
= <oaganian@mail.ru>
2020-10-30 18:31:54 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: 'none',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
